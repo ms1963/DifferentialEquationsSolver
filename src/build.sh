f8adb3615fab572e0dd5120fba00c5055f7382b5
Michael Stal <michael.stal@gmail.com>
2026-01-04 01:05:19 +0100

    Add build script to set up virtual environment

---
+++ src/build.sh
@@ -0,0 +1,14 @@
+# Create virtual environment
+python -m venv venv
+
+# Activate virtual environment
+# On Linux/Mac:
+source venv/bin/activate
+# On Windows:
+venv\Scripts\activate
+
+# Install requirements
+pip install -r requirements.txt
+
+# Run the solver
+python solver.py
